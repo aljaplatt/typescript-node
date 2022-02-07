b68e66c8dfcbcad44d3674fc1c4f0f2d046d8ecc
Alex Platt <alja.platt@gmail.com>
2022-02-07 19:07:51 +0000

    added err middleware

--- dist/src/app.js
+++ dist/src/app.js
@@ -4,5 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const app = express_1.default;
+const todos_1 = __importDefault(require("./routes/todos"));
+const app = (0, express_1.default)();
+app.use("/todos", todos_1.default);
+// error handling middleware
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message,
+    });
+});
 app.listen(3000);
